6e41c7aad9fd29287746dd10a3e14f35db28c6f3
Harit Gandhi <harit.g@zillionica.com>
2024-09-20 02:07:51 +0530

    Cookies in Route Handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,14 +1,19 @@
 import { type NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
     const requestHeaders = new Headers(request.headers);
     const headersList = headers();
+    cookies().set("resultsPerPage", "20");
+    const theme = request.cookies.get("theme");
+    console.log(theme);
     console.log(headersList.get("Authorization"));
     console.log(requestHeaders.get("Authorization"));
+    console.log(cookies().get("resultsPerPage"));
     return new Response("<h1>Profile API data</h1>", {
         headers: {
             "Content-Type": "text/html",
+            "Set-Cookie": `theme=dark`,
         },
     })
 }
