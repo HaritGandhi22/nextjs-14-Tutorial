3879b5fe344abf66756e86d42f8534ae875be1b9
Harit Gandhi <harit.g@zillionica.com>
2024-09-20 02:02:37 +0530

    Headers in Route Handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,14 @@
-export async function GET() {
-    return new Response("Profile API Data");
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+    const requestHeaders = new Headers(request.headers);
+    const headersList = headers();
+    console.log(headersList.get("Authorization"));
+    console.log(requestHeaders.get("Authorization"));
+    return new Response("<h1>Profile API data</h1>", {
+        headers: {
+            "Content-Type": "text/html",
+        },
+    })
 }
