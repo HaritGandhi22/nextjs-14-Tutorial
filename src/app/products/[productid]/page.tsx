6b50c534c6896c5453d84f02087505c73f818877
Harit Gandhi <harit.g@zillionica.com>
2024-09-19 13:53:46 +0530

    Dynamic Routes

---
+++ src/app/products/[productid]/page.tsx
@@ -0,0 +1,11 @@
+export default function ProductDetails({ params }: {
+    params: {
+        productid: string
+    }
+}) {
+    return (
+        <>
+            <h1>Details about Product {params.productid}</h1>
+        </>
+    )
+}
---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+export default function ProductList() {
+  const productId = 100;
+  return (
+    <>
+      <h1>Product List</h1>
+      <h2>Product 1</h2>
+      <h2>Product 2</h2>
+      <h2>Product 3</h2>
+    </>
+  );
+}
