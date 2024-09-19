5d445741fccdfe5834f5cd79900a74732c8033e6
Harit Gandhi <harit.g@zillionica.com>
2024-09-19 14:12:25 +0530

    Not Found Page

--- src/app/products/[productid]/reviews/[reviewid]/page.tsx
+++ src/app/products/[productid]/reviews/[reviewid]/page.tsx
@@ -1,8 +1,13 @@
-export default function ProductDetail({
+import { notFound } from "next/navigation";
+
+export default function ReviewDetail({
     params,
 }: {
     params: { productid: string; reviewid: string };
 }) {
+    if (parseInt(params.reviewid) > 1000) {
+        notFound();
+    }
     return (
         <h1>
             Review {params.reviewid} for product {params.productid}
