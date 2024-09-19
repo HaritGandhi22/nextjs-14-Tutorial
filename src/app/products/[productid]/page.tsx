02ec7e818d31061602b7ff6070f8a6d49ef4c18c
Harit Gandhi <harit.g@zillionica.com>
2024-09-19 16:04:18 +0530

    Routing Metadata

--- src/app/products/[productid]/page.tsx
+++ src/app/products/[productid]/page.tsx
@@ -1,11 +1,23 @@
-export default function ProductDetails({ params }: {
-    params: {
-        productid: string
-    }
-}) {
-    return (
-        <>
-            <h1>Details about Product {params.productid}</h1>
-        </>
-    )
+import { Metadata } from "next";
+
+type Props = {
+  params: { productid: string };
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const { productid } = params;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(productid);
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+export default function ProductDetails({ params }: Props) {
+  return <h1>Details about product {params.productid}</h1>;
 }
