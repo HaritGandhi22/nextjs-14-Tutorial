55fbb8273a9c9a749c311451285bd78ccb5974a5
Harit Gandhi <harit.g@zillionica.com>
2024-09-19 18:43:23 +0530

    Handling a POST Request

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -5,3 +5,13 @@ export async function GET(request: NextRequest) {
 
     return Response.json(comments);
 }
+
+export async function POST(request: Request) {
+    const comment = await request.json();
+    const newComment = { id: comments.length + 1, text: comment.text };
+    comments.push(newComment);
+    return new Response(JSON.stringify(newComment), {
+        headers: { "Content-Type": "application/json" },
+        status: 201,
+    });
+}
