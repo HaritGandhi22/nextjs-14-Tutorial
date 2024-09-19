86a7c3ba79e75cdfbb38fae0b392fd8ae5f2b5d9
Harit Gandhi <harit.g@zillionica.com>
2024-09-19 16:58:22 +0530

    Error Handling

--- src/app/products/[productid]/reviews/[reviewid]/page.tsx
+++ src/app/products/[productid]/reviews/[reviewid]/page.tsx
@@ -1,16 +1,22 @@
+"use client";
 import { notFound } from "next/navigation";
 
-export default function ReviewDetail({
-    params,
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
+export default function ProductDetail({
+  params,
 }: {
-    params: { productid: string; reviewid: string };
+  params: { productId: string; reviewId: string };
 }) {
-    if (parseInt(params.reviewid) > 1000) {
-        notFound();
-    }
-    return (
-        <h1>
-            Review {params.reviewid} for product {params.productid}
-        </h1>
-    );
+
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
+  return (
+    <h1>
+      Review {params.reviewId} for product {params.productId}
+    </h1>
+  );
 }
