5073c6f527ebdd49929ad65f10e55c962d02c0d6
Harit Gandhi <harit.g@zillionica.com>
2024-09-22 20:49:55 +0530

    Third Party Packages

--- src/app/server-route/page.tsx
+++ src/app/server-route/page.tsx
@@ -1,3 +1,4 @@
+import { ImageSlider } from "@/components/image-slider";
 import { serverSideFunction } from "@/utils/server-utils"
 
 export default function ServerRoutePage() {
@@ -7,6 +8,7 @@ export default function ServerRoutePage() {
         <>
             <h1>Server Route Page</h1>
             <p>{result}</p>
+            <ImageSlider />
         </>
     )
 }
