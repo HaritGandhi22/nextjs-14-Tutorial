0abdf5d2167b4c518c4d137c04e4b4a00f896aaa
Harit Gandhi <harit.g@zillionica.com>
2024-09-23 00:10:28 +0530

    Client-only Code

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,14 +1,12 @@
 "use client";
-
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { useTheme } from "@/components/theme-provider";
+import { clientSideFunction } from "@/utils/client-utils";
 
 // import "./ImageSlider.css";
 
 export default function ClientRoutePage() {
   const theme = useTheme();
+  const result = clientSideFunction();
   return (
     <>
       <h1
@@ -16,24 +14,8 @@ export default function ClientRoutePage() {
           color: theme.colors.secondary,
         }}
       >
-        Server Route
+        Client Route {result}
       </h1>
-      <div className="image-slider-container">
-        <Slider>
-          <div>
-            <img src="https://picsum.photos/400/200" alt="placeholder" />
-          </div>
-          <div>
-            <img src="https://picsum.photos/400/300" alt="placeholder" />
-          </div>
-          <div>
-            <img src="https://picsum.photos/400/250" alt="placeholder" />
-          </div>
-          <div>
-            <img src="https://picsum.photos/400/350" alt="placeholder" />
-          </div>
-        </Slider>
-      </div>
     </>
   );
 }
